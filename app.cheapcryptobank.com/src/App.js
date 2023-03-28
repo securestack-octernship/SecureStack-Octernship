4f71b9d8404c3b11e4d2b215de957f4c10b105bd
Brandon Awan <abibangbrandon855@gmail.com>
2023-03-28 15:16:39 +0100

    octernship submission, with found vulnerabilities

---
+++ app.cheapcryptobank.com/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Login from "./components/Login";
+import NavBar from "./components/NavBar";
+function App() {
+
+const password="v6faF+gC7Zs@?VyCqtEYQ%?S"
+  return (
+    <div className="App">
+      <NavBar />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
